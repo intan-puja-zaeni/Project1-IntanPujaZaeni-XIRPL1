2d15866175a4399d7b85643768d7791efcdac8bd
intanpujazaeni <81729119+intan-puja-zaeni@users.noreply.github.com>
2021-06-22 16:21:46 +0700

    Implementasi Kelas Ramen, Kuah, Toping, & Minuman
    Aplikasi Kasir Modul B - D 
    Implementasi kelas struktur aplikasi kasir, menu, daftar menu, dan masih banyak lagi.

---
+++ AplikasiRestauran/Ramen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author K_707
+ */
+public class Ramen extends Menu{
+    
+    public Ramen(String nama_ramen, double harga) { 
+        setNama_menu(nama_ramen);
+        setHarga(harga);
+        setKategori("Ramen");
+    }
+} 
