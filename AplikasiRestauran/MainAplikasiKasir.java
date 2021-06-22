17552330a461c810ef744aa44da105f902b31116
intanpujazaeni <81729119+intan-puja-zaeni@users.noreply.github.com>
2021-06-22 22:49:16 +0700

    Modul D: Pembuatan Pesanan
    Aplikasi Kasir Modul B - D 
    Implementasi kelas struktur aplikasi kasir, menu, daftar menu, dan masih banyak lagi.

--- AplikasiRestauran/MainAplikasiKasir.java
+++ AplikasiRestauran/MainAplikasiKasir.java
@@ -7,9 +7,13 @@ package main;
 
 import classes.DaftarMenu;
 import classes.Kuah;
+import classes.Menu;
 import classes.Minuman;
+import classes.Pesanan;
 import classes.Ramen;
 import classes.Toping;
+import classes.Transaksi;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -24,7 +28,7 @@ public class MainAplikasiKasir {
      public static double BIAYA_SERVICE = 0.05;
      //End Of Tambahkan
      
-    public static void main(String[] args) { 
+    public static void main(String[] args, String level, Pesanan pesaan_kuah) { 
         //init
         Scanner input = new Scanner(System.in);
         //Tambahkan
@@ -32,7 +36,6 @@ public class MainAplikasiKasir {
         String transaksi_lagi = "", pesan_lagi = "", keterangan = "", makan_ditempat;
         int jumlah_pesanan, no_menu;
         //End Of Tambahkan
-        
         
         MainAplikasiKasir app = new MainAplikasiKasir();
         //tampilkan daftar menu
@@ -54,8 +57,77 @@ public class MainAplikasiKasir {
             System.out.print("Nomor Meja : ");
             no_meja = input.next();
         }
-    }
+        
+        //buat transaksi baru
+        Transaksi trans = new Transaksi(no_transaksi, nama_pemesan, tanggal, no_meja);
+        System.out.println("======== PESANAN ========");
+        int no_kuah;
+        do {
+            //ambil menu berdasarkan nomot urut yang dipilih
+            Menu menu_yang_dipilih = app.daftarMenu.pilihMenu();
+            
+            jumlah_pesanan = (int) app.cekInputNumber("Jumlah : ");
+                    
+            //buat pesanan
+            Pesanan pesanan = new Pesanan(menu_yang_dipilih, jumlah_pesanan);
+            trans.tambahPesanan(pesanan);
+            
+            //khusus untuk menu ramen, pesanan kuahnya langsung diinput juga
+            if (menu_yang_dipilih.getKategori().equals("Ramen")) {
+                //looping sesuai jumlah pesanan ramen 
+                int jumlah_ramen = jumlah_pesanan;
+                do {
+                    //ambil objek menu berdasarkan nomor yang dipilih
+                    Menu kuah_yang_dipilih = app.daftarMenu.pilihMenu();
+                    
+                    System.out.print("Level : [0-5]");
+                    String level = input.next();
+                    
+                    //validasi jumlah kuah tidak boleh lebih besar dari jumlah_ramen
+                    int jumlah_kuah = 0;
+                    do {
+                        
+                        jumlah_kuah = (int) app.cekInputNumber("Jumlah : ");
+                        
+                        if (jumlah_kuah > jumlah_ramen) {
+                            System.out.println("[Err] Jumlah kuah melebihi jumlah ramen ");
+                        } else {
+                            
+                            break;
+                        }
+                    } while (jumlah_kuah > jumlah_ramen);
+                    
+                    //set pesanan kuah 
+                    Pesanan pesaan_kuah = new Pesanan(kuah_yang_dipilih, jumlah_kuah};
+                    pesaan_kuah.setKeterangan("Level " + level);
+                    
+                    //tambahkan pesanan kuah ke transaksi
+                    trans.tambahPesanan(pesaan_kuah);
+                    //hitung jumlah ramen yang belum dipesan kuah nya 
+                    jumlah ramen -= jumlah_kuah;
+                } 
+            while (jumlah_rameh > 0);
+                
+            } 
+        else {
+                //jika keterangan tidak diisi tulis - 
+                System.out.print("Keterangan [- jika kosong]: ");
+                keterangan = input.next();
+            }
+            
+            //cek jika keterangan diisi selain "-" set ke pesanan
+            if (!keterangan.equals("-")) {
+                pesanan.setKeterangan(keterangan);
+            }
+            
+            //konfirmasi, mau tambah pesanan atau tidak
+            System.out.print("Tambah Pesanan lagi? [Y/N] : ");
+            pesan_lagi = input.next();
+            while (pesan_lagi.equalsIgnoreCase("Y"));
+        }   
     
+
+        
     public void generateDaftarMenu() { 
         daftarMenu = new DaftarMenu();
         daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
@@ -77,5 +149,38 @@ public class MainAplikasiKasir {
         daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
         
         daftarMenu.tampilDaftarMenu();
+    }
+    
+    public double cekInputNumber(String label) {
+        try{
+            Scanner get_input = new Scanner(System.in);
+            System.out.print(label);
+            double nilai = get_input.nextDouble();
+            
+            return nilai;
+        }catch(InputMismatchException ex){
+            System.out.println("[Err] Harap masukkan angka");
+            return cekInputNumber(label);
+        }
+    }
+
+    private static class pesanan {
+
+        private static void setKeterangan(String keterangan) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public pesanan() {
+        }
+    }
+
+    private static class pesaan_kuah {
+
+        private static void setKeterangan(String string) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public pesaan_kuah() {
+        }
     }
 }
---
+++ AplikasiRestauran/Transaksi.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author K_707
+ */
+import java.util.ArrayList;
+
+public class Transaksi {
+    private String noTransaksi;
+    private String namaPemesan;
+    private String tanggal;
+    private String noMeja;
+    private ArrayList<Pesanan> pesanan; 
+    private double uangBayar; 
+    private double pajak; 
+    private double totalBayar;
+    //tambah 
+    private double biayaService=0;
+    
+    public Transaksi(String no_transaksi, String nm_pemesan, String tanggal, String no_meja) { 
+        this.noTransaksi = no_transaksi;
+        this.namaPemesan = nm_pemesan;
+        this.tanggal = tanggal;
+        this.noMeja = no_meja;
+    
+        pesanan = new ArrayList<>();
+    }
+    
+    public void tambahPesanan(Pesanan pesanan) { 
+        this.pesanan.add(pesanan);
+    }
+    
+    public ArrayList<Pesanan> getSemuaPesanan() { 
+        return pesanan;
+    }
+    
+    public double hitungTotalBayar() { return 0; }
+    
+    public double hitungKembalian() { return 0; }
+    
+    public void cetakStruk() { }
+    
+}
