f9956090c1e2f140c465a5d697a578ab3f4a4fb9
intanpujazaeni <81729119+intan-puja-zaeni@users.noreply.github.com>
2021-06-22 18:42:09 +0700

    Modul D: Input Data Pelanggan
    Aplikasi Kasir Modul B - D 
    Implementasi kelas struktur aplikasi kasir, menu, daftar menu, dan masih banyak lagi.

--- AplikasiRestauran/MainAplikasiKasir.java
+++ AplikasiRestauran/MainAplikasiKasir.java
@@ -37,6 +37,23 @@ public class MainAplikasiKasir {
         MainAplikasiKasir app = new MainAplikasiKasir();
         //tampilkan daftar menu
         app.generateDaftarMenu();
+        //mulai transaksi
+        System.out.println("======== TRANSAKSI ========");
+        
+        //ambil data transaksi
+        System.out.print("No Transaksi : ");
+        no_transaksi = input.next();
+        System.out.print("Pemesan : ");
+        nama_pemesan = input.next();
+        System.out.print("Tanggal : [dd-mm-yyyy] ");
+        tanggal = input.next();
+        System.out.print("Makan ditempat? [Y/N] ");
+        makan_ditempat = input.next();
+        
+        if (makan_ditempat.equalsIgnoreCase("Y")) {
+            System.out.print("Nomor Meja : ");
+            no_meja = input.next();
+        }
     }
     
     public void generateDaftarMenu() { 
